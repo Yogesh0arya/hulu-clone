22efb9b618362671ceefbcb027ee1a08685405ea
Yogesh0arya <yogesh084arya@gmail.com>
2021-05-21 12:02:55 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens:{
+        "3xl":"2000px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('tailwind-scrollbar-hide')],
+}
